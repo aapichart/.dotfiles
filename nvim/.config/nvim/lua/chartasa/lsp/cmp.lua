504aafd9fd23d54a5f081434561a1ccf1fe7bf5d
Chart <chart.asa@gmail.com>
2022-04-11 01:20:24 +0700

    Update code completion setting

--- nvim/.config/nvim/lua/chartasa/lsp/cmp.lua
+++ nvim/.config/nvim/lua/chartasa/lsp/cmp.lua
@@ -11,7 +11,6 @@ end
 
 local lspkind = require('lspkind')
 
-
 local check_backspace = function()
   local col = vim.fn.col "." - 1
   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
@@ -48,22 +47,28 @@ local kind_icons = {
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
 
 local cmp = require'cmp'
 cmp.setup({
   snippet = {
     expand = function(args)
       -- For `vsnip` user.
-      vim.fn["vsnip#anonymous"](args.body)
+      -- vim.fn["vsnip#anonymous"](args.body)
 
       -- For `luasnip` user.
-      -- require('luasnip').lsp_expand(args.body)
+      require('luasnip').lsp_expand(args.body)
 
       -- For `ultisnips` user.
       -- vim.fn["UltiSnips#Anon"](args.body)
     end,
   },
   mapping = {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+	["<C-j>"] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
@@ -72,42 +77,90 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn["vsnip#available"]() == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif check_backspace() then
+        fallback()
       else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        fallback()
       end
     end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function()
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
       end
-    end, { "i", "s" }),
+    end, {"i", "s" }),
+      -- if cmp.visible() then
+        -- cmp.select_next_item()
+      -- elseif vim.fn["vsnip#available"]() == 1 then
+        -- feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      -- elseif has_words_before() then
+        -- cmp.complete()
+      -- else
+        -- fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      -- end
+    -- end, { "i", "s" }),
+
+    -- ["<S-Tab>"] = cmp.mapping(function()
+      -- if cmp.visible() then
+        -- cmp.select_prev_item()
+      -- elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        -- feedkey("<Plug>(vsnip-jump-prev)", "")
+      -- end
+    -- end, { "i", "s" }),
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
   },
 
   sources = {
     { name = 'nvim_lsp' },
 
     -- For vsnip user.
-    { name = 'vsnip' },
+    -- { name = 'vsnip' },
 
     -- For luasnip user.
-    -- { name = 'luasnip' },
+    { name = 'luasnip' },
 
     -- For ultisnips user.
-    -- { name = 'ultisnips' },
+    { name = 'ultisnips' },
 
     { name = 'buffer' },
+    { name = 'Path'}
   },
 
-  formatting = {
-    format = lspkind.cmp_format({with_text = true, maxwidth = 50})
-  }
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  },
+  experimental = {
+    ghost_text = false,
+    native_menu = false,
+  },
+  -- formatting = {
+    -- format = lspkind.cmp_format({with_text = true, maxwidth = 50})
+  -- }
 })
 
 
