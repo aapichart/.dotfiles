29ba614fb8eeebf2fd9ec029f592800f8ab920ea
Chart <chart.asa@gmail.com>
2023-11-13 23:07:12 +0700

    add more config ex. vim-surround, vim-slime

--- nvim.lazy/.config/nvim/lua/utils/lsp.lua
+++ nvim.lazy/.config/nvim/lua/utils/lsp.lua
@@ -1,4 +1,5 @@
-local keymap = vim.keymap
+local mapkey = require("utils.keymapper").mapkey
+-- local keymap = vim.keymap
 
 local M = {}
 
@@ -6,19 +7,21 @@ local M = {}
 M.on_attach = function(client, bufnr)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 
-	keymap.set("n", "<leader>gD", "<cmd>Lspsaga goto_definition<CR>", opts) -- go to definition
-	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
-	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
-	keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show diagnostics for line
-	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
-	keymap.set("n", "<leader>pd", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to prev diagnostic in buffer
-	keymap.set("n", "<leader>nd", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
-	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
-	keymap.set("n", "<leader>lo", "<cmd>LSoutlineToggle<CR>", opts)
-	keymap.set("n", "<leader>gf", "<cmd>Lspsaga lsp_finder<CR>", opts)
+	mapkey("<leader>fd", "Lspsaga finder", "n", opts) -- go to definition
+	mapkey("<leader>gd", "Lspsaga peek_definition", "n", opts) -- peek definition
+	mapkey("<leader>gD", "Lspsaga goto_definition", "n", opts) -- go to definition
+	mapkey("<leader>ca", "Lspsaga code_action", "n", opts) -- see available code actions
+	mapkey("<leader>rn", "Lspsaga rename", "n", opts) -- smart rename
+	mapkey("<leader>D", "Lspsaga show_line_diagnostics", "n", opts) -- show diagnostics for line
+	mapkey("<leader>d", "Lspsaga show_cursor_diagnostics", "n", opts) -- show diagnostics for cursor
+	mapkey("<leader>pd", "Lspsaga diagnostic_jump_prev", "n", opts) -- jump to prev diagnostics in buffer
+	mapkey("<leader>nd", "Lspsaga diagnostic_jump_next", "n", opts) -- jump to next diagnostics in buffer
+	mapkey("K", "Lspsaga hover_doc", "n", opts) -- show documentation for what is under cursor
+	mapkey("<leader>lo", "Lspsaga LSoutlineToggle", "n", opts)
+	mapkey("<leader>gf", "Lspsaga lsp_finder", "n", opts)
 
 	if client.name == "pyright" then
-		keymap.set("n", "<leader>oi", "<cmd>PyrightOrganizeImports<CR>", opts)
+		mapkey("<leader>oi", "PyrightOrganizeImports", "n", opts)
 	end
 end
 
