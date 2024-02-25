53ba0e76cc0adbd17f84b05a8e9fc30e376423c4
Chart <chart.asa@gmail.com>
2024-02-25 19:59:30 +0700

    Change install script

---
+++ installP1.sh
@@ -0,0 +1,29 @@
+if [ $SHELL = "/usr/bin/bash" ] || [ $SHELL = "/bin/bash" ] || [ $SHELL = "/bin/sh" ] || [ $REINSTALL_NIX = "TRUE" ]; then
+    
+    # install nix
+    sudo mkdir /nix
+    sudo chown -R $USER:$USER /nix
+
+    # Install nix 
+    curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
+    # source nix
+    . $HOME/.nix-profile/etc/profile.d/nix.sh
+
+    # install packages
+    nix-env -iA \
+        nixpkgs.antibody \
+        nixpkgs.git \
+        nixpkgs.tmux \
+	      nixpkgs.direnv \
+        nixpkgs.neovim \
+        nixpkgs.stow 
+
+    # Need to reboot the system
+    read -p "Please reboot the system, and then run installP2.sh (y/n)" check
+    if [ "$check" = "y" ]; then
+       reboot
+    else
+       echo "Please reboot system and run installP2.sh!!!!"
+    fi
+fi
+
---
+++ installP2.sh
@@ -0,0 +1,87 @@
+    # install packages
+    nix-env -iA \
+        nixpkgs.zsh \
+        nixpkgs.conky \
+        nixpkgs.terminator \
+        nixpkgs.lf \
+        nixpkgs.gcc \
+        nixpkgs.htop \
+        nixpkgs.yarn \
+        nixpkgs.nodejs_20 \
+        nixpkgs.xsel \
+        nixpkgs.xclip \
+        nixpkgs.fzf \
+        nixpkgs.lazygit \
+        nixpkgs.ripgrep \
+        nixpkgs.bat \
+        nixpkgs.gtop \
+        nixpkgs.gping \
+        nixpkgs.neofetch \
+        nixpkgs.tree \
+        nixpkgs.cmatrix \
+        nixpkgs.nmap \
+        nixpkgs.postgresql \
+        nixpkgs.python310Full \
+        nixpkgs.python310Packages.pip \
+        nixpkgs.virtualenv \
+        nixpkgs.pipenv \
+        nixpkgs.pv \
+        nixpkgs.fd \
+        nixpkgs.ranger \
+        nixpkgs.jq \
+        nixpkgs.nfs-utils \
+        nixpkgs.pandoc \
+        nixpkgs.zathura \
+        nixpkgs.bmon \
+        nixpkgs.iftop \
+        nixpkgs.nload \
+        nixpkgs.vpnc \
+        nixpkgs.luarocks \
+        nixpkgs.figlet \
+        nixpkgs.go \
+        nixpkgs.gotools \
+        nixpkgs.gopls \
+        nixpkgs.go-outline \
+        #nixpkgs.gocode \
+        nixpkgs.gopkgs \
+        #nixpkgs.gocode-gomod \
+        nixpkgs.godef \
+        nixpkgs.golint \
+        nixpkgs.typescript
+
+
+    # add zsh to valid login shells
+    command -v zsh | sudo tee -a /etc/shells
+
+    # stow any dot config modules
+    stow git
+    stow zsh
+    #stow nvim
+    stow nvim.lazy
+    #stow nvim.22
+    stow tmux
+    stow tmuxifier
+    stow lfconf
+    stow direnv
+    stow deskpi
+    stow gtk-3.0
+    stow selected_editor
+    stow autostart
+    stow startupScript
+    stow rasp4utils
+    stow myfonts
+    stow krohnkite
+    stow terminator
+    stow conky
+    
+   # Set up MesloLGS with devicons for vim and many app's glyph
+    sudo cp $HOME/.local/share/fonts/*.ttf /usr/local/share/fonts
+    #sudo cd /usr/local/share/fonts
+    sudo fc-cache -fv
+    
+    # use zsh as default shell
+    chsh -s $(which zsh) $USER
+
+    # bundle zsh plugins
+    antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
