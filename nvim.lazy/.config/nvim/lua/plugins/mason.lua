d3189b61929ec0766e5a86c296cae075b1004cdf
Chart <chart.asa@gmail.com>
2024-09-14 15:33:46 +0700

    update library for typescript language server: conform.nvim

--- nvim.lazy/.config/nvim/lua/plugins/mason.lua
+++ nvim.lazy/.config/nvim/lua/plugins/mason.lua
@@ -1,16 +1,40 @@
 return {
-  "williamboman/mason.nvim",
-  -- lazy = false,
-  cmd = {"Mason", "MasonInstall","MasonUpdate"},
-  event = "BufReadPre",
-  option = {
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗"
-      },
-    },
-  },
-}
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	lazy = false,
+	cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+	event = "BufReadPre",
+	option = {},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
 
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"ts_server",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+			},
+			automatic_installation = true,
+		})
+	end,
+}
