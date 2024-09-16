a0228b9735a13a8039eccfe4e91e99fbf1fc8d59
Chart <chart.asa@gmail.com>
2024-09-16 14:02:45 +0700

    update code completion and insert more snippet for react-native

--- nvim.lazy/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim.lazy/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,11 +1,51 @@
 return {
 	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"onsails/lspkind.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-calc",
+		"hrsh7th/cmp-emoji",
+		"saadparwaiz1/cmp_luasnip",
+		"f3fora/cmp-spell",
+		"ray-x/cmp-treesitter",
+		"kdheepak/cmp-latex-symbols",
+		"jc-doyle/cmp-pandoc-references",
+		"rafamadriz/friendly-snippets",
+		{
+			"L3MON4D3/LuaSnip",
+			-- follow latest release.
+			version = "2.*", -- Replace <CurrentMajor> by the latest release major (first number of lastest release)
+			-- install jsregexp (Optional!)
+			build = "make install_jsregexp",
+		},
+		{
+			"KadoBOT/cmp-plugins",
+			config = function()
+				require("cmp-plugins").setup({
+					files = { "plugins.lua" },
+				})
+			end,
+		},
+	},
+
 	config = function()
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 		local lspkind = require("lspkind")
+		lspkind.init()
 
-		require("luasnip/loaders/from_vscode").lazy_load()
+		local has_words_before = function()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
+		-- for friendly snippets
+		require("luasnip.loaders.from_vscode").lazy_load()
+		-- for custom snippets
+		require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/snips" } })
 
 		vim.opt.completeopt = "menu,menuone,noselect"
 
@@ -24,31 +64,52 @@ return {
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
+			autocomplete = false,
 			-- Source for autocompletion
 			sources = cmp.config.sources({
+				{ name = "otter" },
+				{ name = "plugins" },
+				{ name = "luasnip", keyword_length = 3, max_item_count = 3 },
+				{ name = "pandoc_references" },
+				{ name = "buffer", keyword_length = 5, max_item_count = 3 },
+				{ name = "spell" },
+				{ name = "treesitter", keyword_length = 5, max_item_count = 3 },
+				{ name = "calc" },
+				{ name = "latex_symbols" },
+				{ name = "emoji" },
 				{ name = "nvim_lsp" }, -- lsp
-				{ name = "luasnip" }, -- snippet
-				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
 			}),
 			-- configure lspkind for vs-code like icons
 			formatting = {
 				format = lspkind.cmp_format({
-					maxwidth = 50,
+					with_text = true,
+					maxwidth = 100,
 					ellipsis_char = "...",
+					menu = {
+						otter = "[🦦]",
+						luasnip = "[snip]",
+						nvim_lsp = "[LSP]",
+						buffer = "[buf]",
+						path = "[path]",
+						spell = "[spell]",
+						pandoc_references = "[ref]",
+						tags = "[tag]",
+						treesitter = "[TS]",
+						calc = "[calc]",
+						latex_symbols = "[tex]",
+						emoji = "[emoji]",
+					},
 				}),
+			},
+			view = {
+				entries = "native",
+			},
+			window = {
+				documentation = {
+					border = require("misc.style").border,
+				},
 			},
 		})
 	end,
-
-	dependencies = {
-		"onsails/lspkind.nvim",
-		{
-			"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			version = "2.*", -- Replace <CurrentMajor> by the latest release major (first number of lastest release)
-			-- install jsregexp (Optional!)
-			build = "make install_jsregexp",
-		},
-	},
 }
--- nvim.lazy/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim.lazy/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,39 +1,51 @@
 local config = function()
-  require("nvim-treesitter.configs").setup({
-    indent = {
-      enable = true,
-    },
-    autotag = {
-      enable = true,
-    },
-    ensure_installed = {
-      "markdown",
-      "json",
-      "javascript",
-      "typescript",
-      "yaml",
-      "html",
-      "css",
-      "bash",
-      "lua",
-      "dockerfile",
-      "solidity",
-      "gitignore",
-      "python",
-      "vue",
-      "sql",
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = true,
-    },
-  })
+	require("nvim-treesitter.configs").setup({
+		indent = {
+			enable = true,
+		},
+		autotag = {
+			enable = true,
+		},
+		ensure_installed = {
+			"markdown",
+			"r",
+			"julia",
+			"vim",
+			"query",
+			"json",
+			"javascript",
+			"typescript",
+			"yaml",
+			"html",
+			"css",
+			"bash",
+			"lua",
+			"dockerfile",
+			"solidity",
+			"gitignore",
+			"python",
+			"vue",
+			"sql",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = true,
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn",
+				node_incremental = "grn",
+				scope_incremental = "grc",
+				node_decremental = "grm",
+			},
+		},
+	})
 end
 
-
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  config = config
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	config = config,
 }
