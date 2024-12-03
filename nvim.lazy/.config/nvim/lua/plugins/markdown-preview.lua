ca6fc761a775188dc09e10d0deaa3671e05243ab
Apichart Asavarojpanich <chart.asa@gmail.com>
2024-12-03 10:22:41 +0700

    update obsidian config4

--- nvim.lazy/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim.lazy/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,11 @@
-return{
-  'iamcco/markdown-preview.nvim',
-  cmd = { "MarkdownPreviewToggle"},
-  ft = { "markdown"},
-  build = function() vim.fn["mkdp#util#install"]() end,
- }
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && npm install",
+	keys = {},
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+	ft = { "markdown" },
+	-- build = function() vim.fn["mkdp#util#install"]() end,
+}
