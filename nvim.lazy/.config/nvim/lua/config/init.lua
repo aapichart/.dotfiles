ca771c53cfb93ac33314ffeb37b8e877e89cf626
Apichart Asavarojpanich <chart.asa@gmail.com>
2024-12-17 11:15:23 +0700

    update AI prompt using Avante

--- nvim.lazy/.config/nvim/lua/config/init.lua
+++ nvim.lazy/.config/nvim/lua/config/init.lua
@@ -39,15 +39,4 @@ local opts =
 		change_detection = {
 			notify = true,
 		},
-		backend = "kitty",
-		integrations = {
-			markdown = {
-				enabled = true,
-				clear_in_insert_mode = false,
-				download_remote_images = true,
-				only_render_image_at_cursor = false,
-				filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-			},
-		},
-	}, 
-require("lazy").setup("plugins", opts)
+	}, require("lazy").setup("plugins", opts)
