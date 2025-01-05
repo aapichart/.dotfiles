e617a2373e5e30d8233c4f81b3998b767880a415
Apichart Asavarojpanich <chart.asa@gmail.com>
2025-01-06 00:29:32 +0700

    update nvim provide-python setup

--- nvim.lazy/.config/nvim/lua/config/globals.lua
+++ nvim.lazy/.config/nvim/lua/config/globals.lua
@@ -2,10 +2,11 @@
 vim.g.mapleader = " " -- Make sure to set 'mapleader' before lazy so your mapping are correct
 vim.g.maplocalleader = " "
 vim.g.tree_sitter_cli_path = "$HOME/.nix-profile/bin/"
-vim.g.python3_host_prog = "$HOME/.local/bin/python3"
+-- vim.g.python3_host_prog = "$HOME/.local/bin/pythonNvim/.venv/bin/python3"
+vim.g.python3_host_prog = "/usr/bin/python3"
 -- vim.g.python3_host_prog = "$HOME/.nix-profile/bin/python3.13"
--- vim.g.node_host_prog = "$HOME/.nix-profile/bin/npm"
-vim.g.node_host_prog = "$HOME/.nvm/versions/node/v23.5.0/bin/node"
+vim.g.node_host_prog = vim.fn.expand("~") .. "/.nvm/versions/node/v23.5.0/lib/node_modules/neovim/bin/cli.js"
+-- vim.g.node_host_prog = "$HOME/.nvm/versions/node/v23.5.0/bin/node"
 vim.g.loaded_node_provider = 1
 --
 -- This setting is for Avante
