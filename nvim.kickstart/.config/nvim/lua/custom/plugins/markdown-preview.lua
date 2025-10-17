161b5355cd5c4dced5b793b1224a16f33bfb9ebc
Apichart Asavarojpanich <chart.asa@gmail.com>
2025-10-17 23:34:04 +0700

    update tmuxinator/coding.yml for initiate coding environment

--- nvim.kickstart/.config/nvim/lua/custom/plugins/markdown-preview.lua
+++ nvim.kickstart/.config/nvim/lua/custom/plugins/markdown-preview.lua
@@ -1,13 +1,17 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install",
-	keys = {},
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
-	-- build = function()
-	-- 	vim.fn["mkdp#util#install"]()
-	-- end,
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && npm install',
+  keys = {},
+  init = function()
+    vim.g.mkdp_filetypes = { 'markdown' }
+  end,
+  ft = { 'markdown' },
+  -- build = function()
+  -- 	vim.fn["mkdp#util#install"]()
+  -- end,
+  requires = {
+    'zhaozg/vim-diagram', -- Syntax plugin for Mermaid.js diagrams
+    'aklt/plantuml-syntax', -- If you also use PlantUML
+  },
 }
