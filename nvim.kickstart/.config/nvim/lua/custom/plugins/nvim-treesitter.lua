2ad10e1c31ec74c1e49cb0df8a4ebd5f60b69d1d
Apichart Asavarojpanich <chart.asa@gmail.com>
2026-01-21 00:38:59 +0700

    update config for nvim-ts-autotag to auto close tag on typescript files (tsx)

--- nvim.kickstart/.config/nvim/lua/custom/plugins/nvim-treesitter.lua
+++ nvim.kickstart/.config/nvim/lua/custom/plugins/nvim-treesitter.lua
@@ -1,52 +1,64 @@
 local config = function()
-	require("nvim-ts-autotag").setup({
-		indent = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
-		ensure_installed = {
-			"markdown",
-			"markdown_inline",
-			"r",
-			"regex",
-			"julials",
-			"vim",
-			"json",
-			"javascript",
-			"typescript",
-			"yaml",
-			"html",
-			"css",
-			"bash",
-			"lua",
-			"dockerfile",
-			"solidity",
-			"gitignore",
-			"python",
-			"vue",
-			"sql",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "gnn",
-				node_incremental = "grn",
-				scope_incremental = "grc",
-				node_decremental = "grm",
-			},
-		},
-	})
+  require('nvim-ts-autotag').setup {
+    indent = {
+      enable = true,
+    },
+    autotag = {
+      enable = true,
+    },
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = true,
+    },
+    per_filetype = {
+      ['html'] = { enable_close = true },
+      ['typescriptreact'] = { enable_close = true, enable_rename = true, enable_close_on_slash = true },
+      ['javascriptreact'] = { enable_close = true, enable_rename = true, enable_close_on_slash = true },
+    },
+    ensure_installed = {
+      'markdown',
+      'markdown_inline',
+      'r',
+      'regex',
+      'julials',
+      'vim',
+      'json',
+      'javascript',
+      'typescript',
+      'yaml',
+      'html',
+      'css',
+      'bash',
+      'lua',
+      'dockerfile',
+      'solidity',
+      'gitignore',
+      'python',
+      'vue',
+      'sql',
+      'tsx',
+      'jsx',
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = 'gnn',
+        node_incremental = 'grn',
+        scope_incremental = 'grc',
+        node_decremental = 'grm',
+      },
+    },
+  }
 end
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	config = config,
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  config = config,
 }
