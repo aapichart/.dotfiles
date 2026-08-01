64776ae55a3e9524c404c1140fbafeaa95a20575
Apichart Asavarojpanich <chart.asa@gmail.com>
2026-08-02 01:19:30 +0700

    update config for flutter by installing coc-flutter from coc.nvim

--- nvim.kickstart/.config/nvim/lua/custom/plugins/coc.lua
+++ nvim.kickstart/.config/nvim/lua/custom/plugins/coc.lua
@@ -21,12 +21,12 @@ return {
       local col = vim.fn.col '.' - 1
       return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
     end
-    vim.keymap.set('i', '<TAB>', 'coc#pum#visible()?coc#pum#next(1):v:lua.check_back_space()?"<TAB>":coc#refresh()', opts)
-    vim.keymap.set('i', '<S-TAB>', [[coc#pum#visible()?coc#pum#prev(1):"<C-h>"]], opts)
+    vim.keymap.set('i', '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+    vim.keymap.set('i', '<S-TAB>', [[coc#pum#visible() ? coc#pum#prev(1): "<C-h>"]], opts)
 
     -- 2. Confirm selection with Enter/Return
 
-    vim.keymap.set('i', '<CR>', [[coc#pum#visible()?coc#pum#confirm():"<C-g>u<CR><c-r>=coc#on_enter()<CR>"]], opts)
+    vim.keymap.set('i', '<CR>', [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
     -- 3. Standard Navigation Maps(Go to Definition, Reference etc.)
 
